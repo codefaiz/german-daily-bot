5e61f8390daadb462cc245ea19af17e1a8af5c05
codefaiz <89249965+codefaiz@users.noreply.github.com>
2026-02-04 14:03:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,21 @@
+import requests
+import os
 
+TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+message = """
+German Daily Bot 🇩🇪
+Hallo = Hello
+Ich bin = I am
+Danke = Thank you
+"""
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+data = {
+    "chat_id": CHAT_ID,
+    "text": message
+}
+
+requests.post(url, data=data)
